68b85def585ee052e1976f52cd8165154b3c6d13
Mikko Ruuskanen <mikko.ruuskanen@mbnet.fi>
2011-11-08 23:45:09 +0200

    prod.rb2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,8 @@ Djsbestfriend::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 	
-	config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
-
   # Generate digests for assets URLs
   config.assets.digest = true
 
