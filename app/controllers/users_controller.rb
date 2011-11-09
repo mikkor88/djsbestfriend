7360280b53407f1af76cc1f9ccf607aa1490e0c0
Mikko Ruuskanen <mikko.ruuskanen@mbnet.fi>
2011-11-09 18:26:01 +0200

    finished w signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,31 @@
 class UsersController < ApplicationController
-
+	before_filter :authenticate, :except => [:show, :new, :create]
+	
+	def index
+		@users = User.paginate(:page => params[:page])
+		@title = "All users"
+	end
+	
   def new
-    @title = "Sign up"
+    @user = User.new
+		@title = "Sign up"
   end
 	
 	def show
     @user = User.find(params[:id])
 		@title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to DJs Best Friend!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+			@user.password.clear
+			@user.password_confirmation.clear
+    end
   end
 end
